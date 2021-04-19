b4ff4abb89e2e3f74ee5ebd214caebe3ceb38610
kairis7854 <kairis7854@gmail.com>
2021-04-20 01:39:21 +0800

    'updata'

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import App from './App';
 
+
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={ process.env.PUBLIC_URL }>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App/>
     </BrowserRouter>
   </Provider>,
