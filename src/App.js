90963d1e57affaf78714fc1ee75eb43736f5b13d
kairis7854 <kairis7854@gmail.com>
2021-04-20 18:12:18 +0800

    'updata'

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import {Switch,Route,Redirect} from 'react-router-dom'
 import Login from './contaners/login'
 import Admin from './contaners/admin'
 import './App.less';
-import { Router } from 'express';
 
 export default class App extends Component{
   render(){
     return(
       <div className='app'>
-          <Switch>
-            <Route path='/login' component={Login}/>
-            <Route path='/admin' component={Admin}/>
-            <Redirect to='/admin/home'/>
-          </Switch> 
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/admin' component={Admin}/>
+          <Redirect to='/admin/home'/>
+        </Switch>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import {Provider} from 'react-redux'
 import store from './redux/store'
 import App from './App';
 
-
-
-
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App/>
     </BrowserRouter>
   </Provider>,
